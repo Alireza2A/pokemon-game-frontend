845b7535e674d12b08c49cc492b976ab5d4b9644
Julia Baur <juliabaur1@gmx.de>
2025-03-17 16:48:18 +0100

    added poke api url to api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,38 +1,32 @@
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000"; 
+const API_URL = "https://pokeapi.co/api/v2/pokemon"; 
 
-// Function to call Pokemon(s)
+// Function to call a Pokémon
 export const fetchPokemons = async () => {
   try {
-    const response = await fetch(`${API_URL}/pokemon`);
+    const response = await fetch(API_URL); // PokeAPI Endpoint for Pokémon
     if (!response.ok) {
       throw new Error("Failed to fetch Pokémon");
     }
-    return await response.json(); 
+    const data = await response.json(); 
+    return data.results; // Pokemon data are in "results"-Array
   } catch (error) {
     console.error("Error fetching Pokémon:", error);
     throw error;
   }
 };
 
-// Function to add a Pokémon
-export const addPokemon = async (pokemonData) => {
+// Function to call a specific Pokémon
+export const fetchPokemonDetails = async (pokemonName) => {
   try {
-    const response = await fetch(`${API_URL}/pokemon`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(pokemonData),
-    });
-
+    const response = await fetch(`${API_URL}/${pokemonName}`); // Call a single Pokemon
     if (!response.ok) {
-      throw new Error("Failed to add Pokémon");
+      throw new Error("Failed to fetch Pokémon details");
     }
-
-    return await response.json(); // Returning added Pokémon
+    const data = await response.json();
+    return data; 
   } catch (error) {
-    console.error("Error adding Pokémon:", error);
+    console.error("Error fetching Pokémon details:", error);
     throw error;
   }
 };
