6b3ebb37a13fc375698a80e3084575be016c12ff
Ivan <ivan.kels@gmail.com>
2025-03-20 09:21:55 +0100

    adding caught pokemons context

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -7,53 +7,54 @@ import "react-toastify/dist/ReactToastify.css";
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-    const navigate = useNavigate();
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const login = async (formData) => {
-        try {
-            const data = await getUserFromApi(formData);
-            localStorage.setItem("token", data.token);
-            setUser(data.user);
-            navigate("/");
-        } catch (error) {
-            const errorMessage = error.response?.data?.message || "Something went wrong!";
-            toast.error(errorMessage, { position: "top-right", autoClose: 3000 });
-            console.error(error);
-            setUser(null);
-        } finally {
-            setLoading(false);
-        }
+  const login = async (formData) => {
+    try {
+      const data = await getUserFromApi(formData);
+      localStorage.setItem("token", data.token);
+      setUser(data.user);
+      navigate("/");
+    } catch (error) {
+      const errorMessage =
+        error.response?.data?.message || "Something went wrong!";
+      toast.error(errorMessage, { position: "top-right", autoClose: 3000 });
+      console.error(error);
+      setUser(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+    navigate("/login");
+  };
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const data = await profile();
+        setUser(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
     };
+    getUser();
+  }, []);
 
-    const logout = () => {
-        localStorage.removeItem("token");
-        setUser(null);
-        navigate("/login");
-    };
-
-    useEffect(() => {
-        const getUser = async () => {
-            try {
-                const data = await profile();
-                setUser(data);
-            } catch (error) {
-                console.error(error);
-            } finally {
-                setLoading(false);
-            }
-        };
-        getUser();
-    }, []);
-
-    return (
-        <AuthContext.Provider value={{ user, loading, login, logout }}>
-            {children}
-            {/* Ensure ToastContainer is included so toasts work globally */}
-            <ToastContainer />
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{ user, loading, login, logout }}>
+      {children}
+      {/* Ensure ToastContainer is included so toasts work globally */}
+      <ToastContainer />
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
